94c6d46725ca544f7cb27b7f0ccf3f4191f96f0a
lcking992 <lcking992@gmail.com>
2026-01-30 17:22:49 -0500

    Database Problems

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,19 @@
 
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+
+const connectionString = process.env.DATABASE_URL as string
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
+export const prisma = globalForPrisma.prisma || (() => {
+    const pool = new Pool({ connectionString })
+    const adapter = new PrismaPg(pool)
+    return new PrismaClient({
+        adapter,
+        log: ['query']
     })
+})()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
