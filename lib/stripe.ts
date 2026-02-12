e8a5c5eb36a4eb80bf2875572d291fc7aa3cca6b
lcking992 <lcking992@gmail.com>
2026-02-12 17:59:54 -0500

    Stripe Account Implementation

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 
 import Stripe from 'stripe';
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY || '';
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_building';
 
 export const stripe = new Stripe(stripeSecretKey, {
     apiVersion: '2026-01-28.clover' as any,
