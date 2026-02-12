8a24787a96693a857ffb1bbc30b2d5c3b2a43296
lcking992 <lcking992@gmail.com>
2026-02-12 12:32:35 -0500

    Google Cloud Implementation

--- lib/maps-config.ts
+++ lib/maps-config.ts
@@ -3,4 +3,4 @@ export const GOOGLE_MAPS_LIBRARIES: ("places" | "drawing" | "geometry" | "visual
 
 export const GOOGLE_MAPS_SCRIPT_ID = 'google-map-script';
 
-export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
+export const GOOGLE_MAPS_API_KEY = (process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "").trim();
