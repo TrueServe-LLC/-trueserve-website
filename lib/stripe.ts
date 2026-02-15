ab66f440db6170dfa0507efb4e19bcc9871d8c5b
lcking992 <lcking992@gmail.com>
2026-02-15 15:47:07 -0500

    Testing

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,7 @@ import Stripe from 'stripe';
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_building';
 
+// The stripe SDK version ^20.3.0 requires the '2026-01-28.clover' API version types.
 export const stripe = new Stripe(stripeSecretKey, {
-    apiVersion: '2024-06-20',
+    apiVersion: '2026-01-28.clover',
 });
